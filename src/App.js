7542bf90a900f3f34ed62473e5ec2398a574b17c
Siyun Lee <siyun1221@gmail.com>
2022-03-30 10:58:15 +0900

    #7.7 Publishing

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/hello" element={<h1>Hello</h1>} />
         <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
       </Routes>
     </Router>
   );
