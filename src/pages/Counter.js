e7bd812f63db64b9e9090e771ae7bf0c27150029
Siyun Lee <siyun1221@gmail.com>
2022-08-16 16:06:01 +0900

    Renewal Counter App

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,14 +1,37 @@
 import { useState } from "react";
+import styled from "styled-components";
+import * as TbIcons from "react-icons/tb";
+
+const CounterStyle = styled.div`
+  h3 {
+    text-align: center;
+  }
+  button {
+    background: inherit;
+    border: none;
+    cursor: pointer;
+    font-size: 50px;
+    padding: 18px 30px;
+    &:hover {
+      font-size: 53px;
+      padding: 16.5px 30px;
+    }
+  }
+`;
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter((current) => current + 1);
+  const onClick = () => {
+    setCounter((current) => current + 1);
+  };
 
   return (
-    <div>
-      <h1>Total clicks:{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
+    <CounterStyle>
+      <h3>Total click: {counter}</h3>
+      <button onClick={onClick}>
+        <TbIcons.TbHandClick />
+      </button>
+    </CounterStyle>
   );
 };
 
